27656b30205c56de1dd514bd9692a5e3a5bda5e8
Krishna <krishkal9294@gmail.com>
2025-05-09 13:09:23 +0530

    updated the server 5000 to render

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -68,7 +68,7 @@ const LoginPage = ({ setIsLoggedIn, loadUserWishlist }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/api/users/login', {
+      const res = await fetch('https://arcadevault-4.onrender.com/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
--- src/pages/Login/SignupPage.jsx
+++ src/pages/Login/SignupPage.jsx
@@ -74,7 +74,7 @@ const SignupPage = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/signup', {
+      const response = await fetch('https://arcadevault-4.onrender.com/api/users/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
